9cd63a6021fde8c4847e185f6f268de181dac6ab
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2022-11-30 14:59:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading3Constructor
+{
+    internal class Program
+    {
+        static void test()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Test 1: " + i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart obj = new ThreadStart(test); //Single parameter constructor
+            Thread T1 = new Thread(obj);
+            T1.Start();
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
