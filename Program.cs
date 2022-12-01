8f32581520f5b615a14cb543690db55c12697c39
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2022-12-01 17:58:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling_Throw_
+{
+
+
+    public class DivideByOddNumberException : ApplicationException
+    {
+        public override string Message
+        {
+            get { return "Attempted to divide from odd number"; }
+        }
+    }
+    
+    internal class ThrowDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number ");
+            int b=Convert.ToInt32(Console.ReadLine());
+            
+            if (b % 2> 0)
+            {
+                throw new DivideByOddNumberException();
+            }
+            int c = a / b;
+            Console.WriteLine(c);
+            Console.WriteLine("end of the program ");
+        }
+    }
+}
