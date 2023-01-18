c83f1b1f047fc5e7f96e7b16a4d1eee244db9ac1
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2023-01-18 21:40:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachingCharInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string 1 ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter the string 2 ");
+            string str2=Console.ReadLine();
+            string res="";
+            for(int i = 0; i <str1.Length; i++)
+            {
+                for(int j = 0; j <str2.Length; j++)
+                {
+                    if (str1[i] == str2[j])
+                    {
+                        res+=str1[i];
+                    }
+                }
+            }
+            Console.WriteLine(res);   //Case Sensetive
+        }
+    }
+}
