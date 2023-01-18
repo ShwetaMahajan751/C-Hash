3c496a7c6254130ce8e08a376279bdd8231e5941
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2023-01-18 21:45:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string str1=Console.ReadLine();
+            string rev = "";
+            int n = str1.Length;
+            while(n>0)
+            {
+                rev += str1[n - 1];
+                n--;
+            }
+            Console.WriteLine(rev);
+           
+
+
+        }
+    }
+}
