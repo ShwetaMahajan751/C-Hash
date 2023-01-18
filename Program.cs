d73a208bc532a6bb1d22044fff451355449f38ad
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2023-01-18 21:43:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string str=Console.ReadLine();
+            int n = str.Length;
+            string rev = "";
+            while(n>0)
+            {
+                rev+=str[n-1];
+                n--;
+            }
+            Console.WriteLine("The reversed string is: "+rev);
+            if (str == rev)
+            {
+                Console.WriteLine("The string is palindrome ");
+            }
+            else
+            {
+                Console.WriteLine("The string is not palindrome ");
+            }
+        }
+    }
+}
