2cc1e90fda56d13f715ae8d32e34b078f520974c
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2023-02-01 16:23:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default_Constructor
+{
+    public class Employee
+    {
+        public int id;
+        public string name;
+/*        public Employee(string Name,int Id)
+        {
+           name=Name ;
+           id = Id;
+        }*/
+        public  Employee()
+        {
+            Console.WriteLine("Default ");
+           
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Employee e1 = new Employee("Shweta",3);
+            Employee e2 = new Employee("Suku", 4);*/
+            Employee e3 = new Employee();
+
+           
+        }
+    }
+}
