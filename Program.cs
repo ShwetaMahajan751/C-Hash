c863ef0793f947c48e0e48623f1e47d951e6327e
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2023-02-02 12:28:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+     public class Name
+{
+      public string name
+    {
+        get; set;   
+    }
+      
+}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Name nam = new Name();
+        nam.name = "Shweta";
+        Console.WriteLine(nam.name);
+          
+        }
+    }
+
