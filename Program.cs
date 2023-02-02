6f9b9ed936c85937db782ace5370a2a740c67c1b
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2023-02-02 12:30:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AccessSpecifiers  
+{
+    public class Student
+    {
+        // Creating setter and getter for each property  
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+            Student student = new Student();  
+            // Setting values to the properties  
+            student.ID = "101";  
+            student.Name = "Mohan Ram";  
+            student.Email = "mohan@example.com";  
+            // getting values  
+            Console.WriteLine("ID = "+student.ID);  
+            Console.WriteLine("Name = "+student.Name);  
+            Console.WriteLine("Email = "+student.Email);  
+        }  
+    }  
+}  
